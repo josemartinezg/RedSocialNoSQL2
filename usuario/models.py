080a52fa88a721049e5abd7b6dc4794d052d04cd
José M. Martínez García <jmlmartinezg@outlook.com>
2019-04-11 00:22:46 -0400

    NoHayGranVaina

--- usuario/models.py
+++ usuario/models.py
@@ -1,3 +1,24 @@
+import datetime
+
 from django.db import models
 
-# Create your models here.
+# nombre = models.CharField(max_length=200)
+# apellido = models.CharField(max_length=200)
+# nombre_usuario = models.CharField(max_length=200)
+# correo_electronico = models.CharField(max_length=200)
+# fecha_nacimiento = models.DateField()
+# contrasena = models.CharField(max_length=200)
+# sexo = models.CharField(max_length=2)
+# descripcion = models.TextField(blank=True)
+# #pais_nacimiento = CountryField()
+# telefono = models.CharField(max_length=20)
+# es_verificado = models.BooleanField(default='false')
+# es_privado = models.BooleanField(default='false')
+# foto_perfil = models.ImageField(upload_to='photos/%Y/%m/%d/')
+# coordenadas = models.CharField(max_length=200)
+# is_mvp = models.BooleanField(default=False)
+# hire_datte = models.DateTimeField(default=datetime.now, blank=True)
+
+#
+# def __str__(self):
+#     return self.name
